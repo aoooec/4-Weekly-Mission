4162a1b5988d77fde6fbfa74ca4e7b4bbef8c751
aoooec <foottable@gmail.com>
2024-05-12 23:27:09 +0900

    add: tanstack query 설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "@/styles/reset.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
 }
